7ed71d54f64720ebeea0b23541b75f0144f81c5d
DziadDawid <dawidfol@op.pl>
2017-04-02 20:38:46 +0200

    Dodanie recaptch'y

--- signup.php
+++ signup.php
@@ -5,6 +5,7 @@
 	<title>Strona główna banku WYSiWYG</title>
 	<link rel="stylesheet" href="stylesheet/stylesignup.css"></style>
 	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	<script src='https://www.google.com/recaptcha/api.js'></script>
 </head>
 <body bgcolor="#383838">
 	<div id="box">
@@ -18,6 +19,7 @@
 					<input type="text" name="email" placeholder="E-mail"> <br>
 					<input type="password" name="pass" placeholder="Hasło"> <br>
 					<input type="password" name="pass2" placeholder="Powtórz hasło"> <br>
+					<div class="g-recaptcha" data-sitekey="6LcTyxgUAAAAAJpf9gOweWyWgBRmiGz8C_rZLGlK"></div> <br> 
 					<input type="submit" value="Zarejestruj się!">						
 				</form>
 			</div>
