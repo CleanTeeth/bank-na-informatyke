79629c59d2610862a42171a88d5a143865955d2f
DziadDawid <dawidfol@op.pl>
2017-04-03 11:14:53 +0200

    Zamienione signup.php na signupindex.php

--- signup.php
+++ signup.php
@@ -1,29 +1,17 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Strona główna banku WYSiWYG</title>
-	<link rel="stylesheet" href="stylesheet/stylesignup.css"></style>
-	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-	<script src='https://www.google.com/recaptcha/api.js'></script>
-</head>
-<body bgcolor="#383838">
-	<div id="box">
-		<br>
-		<div id="login_space"><br>
-		<pre>		DARMOWA REJESTRACJA DO BANKU WYSiWYG</pre>
-			<div id="enter_info">
-				<form action="login.php" method="post">
-				<input type="text" name="name" placeholder="Imię"> <br>
-				<input type="text" name="last_name" placeholder="Nazwisko"> <br>
-					<input type="text" name="email" placeholder="E-mail"> <br>
-					<input type="password" name="pass" placeholder="Hasło"> <br>
-					<input type="password" name="pass2" placeholder="Powtórz hasło"> <br>
-					<div class="g-recaptcha" data-sitekey="6LcTyxgUAAAAAJpf9gOweWyWgBRmiGz8C_rZLGlK"></div> <br> 
-					<input type="submit" value="Zarejestruj się!">						
-				</form>
-			</div>
-		</div>
-	</div>
-</body>
-</html>
+<?php 
+session_start();
+
+	$imie = $_POST['name'];	
+	$nazwisko = $_POST['last_name'];	
+	$email = $_POST['email'];	
+	$pass1 = $_POST['pass'];	
+	$pass2 = $_POST['pass2'];
+
+	if ($pass1 != $pass2)
+	{
+		$_SESSION['wrongpassword'] = true;
+		$GLOBALS['wrongpasswordglobal'] = "Hasła się różnią.";
+	}
+
+
+?>
