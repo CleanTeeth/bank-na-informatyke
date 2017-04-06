4e7ba721577d530ba6f81918f1e371840f4ad7ac
DziadDawid <dawidfol@op.pl>
2017-04-06 21:47:25 +0200

    numer klienta
    Pokaz numeru klienta  uzytkownikowi

---
+++ clientnumber.php
@@ -0,0 +1,13 @@
+<?php 
+session_start();
+if($_SESSION['islogin'] == true)
+{
+	header('Location: index.php');
+}
+else
+{
+	header('Location: index.php');
+}
+	echo "Twój numer klienta: <b>" . $_SESSION['numerk'] . "</b> zapamiętaj go!";
+	echo "<a href='logout.php'>Wróć</a>";
+ ?>
